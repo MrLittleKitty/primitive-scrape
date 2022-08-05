a955ba54dc4938ddc6b819e80002ccd833f77b43
Mr_Little_Kitty <nuclearcat1337@gmail.com>
2022-08-05 10:44:59 -0700

    Update button styling and functionality

--- src/components/ContextBlockComponent.tsx
+++ src/components/ContextBlockComponent.tsx
@@ -11,22 +11,43 @@ interface ContextBlockComponentProps {
 }
 
 interface ContextBlockComponentState {
-
+    elevation: number,
+    backgroundColor: string
 }
 
 export default class ContextBlockComponent extends React.Component<ContextBlockComponentProps, ContextBlockComponentState> {
 
     constructor(props: ContextBlockComponentProps) {
         super(props);
+        this.state = {
+            elevation: 0,
+            backgroundColor: "#ececec"
+        }
+    }
+
+    isMouseOver = (isMouseOver: boolean) => {
+        const newBackgroundColor = isMouseOver ? this.state.backgroundColor : "#ececec"
+        this.setState({
+            elevation: isMouseOver ? 1 : 0,
+            backgroundColor: newBackgroundColor
+        })
+    }
+
+    isMouseDown = (isMouseDown: boolean) => {
+        const newElevation = isMouseDown ? 0 : 1
+        this.setState({
+            backgroundColor: isMouseDown ? "#c9c9c9" : "#ececec",
+            elevation: newElevation
+        })
     }
 
     render() {
         return (
             <Paper
-                elevation={0}
+                elevation={this.state.elevation}
                 sx={{
                     ...this.props.sx,
-                    backgroundColor: "#ececec"
+                    backgroundColor: this.state.backgroundColor
                 }}
             >
                 <Box sx={{
@@ -40,8 +61,17 @@ export default class ContextBlockComponent extends React.Component<ContextBlockC
                         sx={{
                             flex: 1,
                             minWidth: "100%",
+                            '&:hover': {
+                                backgroundColor: this.state.backgroundColor
+                            }
                         }}
-                        onClick={() => this.props.contextBlockClicked(this.props.context)}>
+                        disableRipple={true}
+                        disableFocusRipple={true}
+                        onMouseDown={() => this.isMouseDown(true)}
+                        onMouseUp={() => this.isMouseDown(false)}
+                        onClick={() => this.props.contextBlockClicked(this.props.context)}
+                        onMouseOver={() => this.isMouseOver(true)}
+                        onMouseOut={() => this.isMouseOver(false)}>
                         <Typography sx={{
                             color: "#404040"
                         }}>
